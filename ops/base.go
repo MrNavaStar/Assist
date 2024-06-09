1d8a322c4f83da372d44da0d6fafc0bc72e5d242
MrNavaStar <Mr.NavaStar@gmail.com>
2024-06-09 13:00:54 -0600

    new op

--- ops/base.go
+++ ops/base.go
@@ -1,5 +1,7 @@
 package ops
 
+import "reflect"
+
 // Ternary is the standard ternary operator which returns posVal
 // if the boolean condition is true, otherwise negVal.
 func Ternary(cond bool, posVal interface{}, negVal interface{}) interface{} {
@@ -7,4 +9,8 @@ func Ternary(cond bool, posVal interface{}, negVal interface{}) interface{} {
 		return posVal
 	}
 	return negVal
+}
+
+func IsEmpty(i interface{}) bool {
+	return reflect.ValueOf(i).Len() == 0
 }
