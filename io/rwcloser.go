1b4f2b2ede54520dbeb2887b5ae101b84cde0273
MrNavaStar <mr.navastar@gmail.com>
2024-08-02 21:35:18 -0600

    Add RWCloser

---
+++ io/rwcloser.go
@@ -0,0 +1,17 @@
+package io
+
+import "io"
+
+// Merges a ReadCloser and a WriteCloser into one
+type RWCloser struct {
+	io.ReadCloser
+	io.WriteCloser
+}
+
+func (rw rwCloser) Close() error {
+	err := rw.ReadCloser.Close()
+	if err := rw.WriteCloser.Close(); err != nil {
+		return err
+	}
+	return err
+}
