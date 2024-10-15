e559c2f360375149862ac454e3139361b7c5941d
MrNavaStar <mr.navastar@gmail.com>
2024-10-14 20:05:48 -0600

    fix ternary

--- ops/ops.go
+++ ops/ops.go
@@ -4,7 +4,7 @@ import "reflect"
 
 // Ternary is the standard ternary operator which returns posVal
 // if the boolean condition is true, otherwise negVal.
-func Ternary(cond bool, posVal interface{}, negVal interface{}) interface{} {
+func Ternary(cond bool, posVal any, negVal any) any {
 	if cond {
 		return posVal
 	}
