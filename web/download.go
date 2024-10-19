9ca8d2f8ba471515d8b025dc8dd07015e6c59534
MrNavaStar <mr.navastar@gmail.com>
2024-10-18 22:53:16 -0600

    make download api simpler

--- web/download.go
+++ web/download.go
@@ -2,23 +2,24 @@ package web
 
 import (
 	"fmt"
-	"github.com/mrnavastar/assist/fs"
 	"io"
 	"net/http"
 	"os"
+
+	"github.com/mrnavastar/assist/fs"
 )
 
-// Download downloads a file at the given path with the given name from a url if the file does not exist
-func Download(filepath string, filename string, url string) (err error) {
-	if fs.Exists(filepath + "/" + filename) {
+// Download downloads a file at the given path from a url if the file does not exist
+func Download(file string, url string) (err error) {
+	if fs.Exists(file) {
 		return nil
 	}
-	if err := os.MkdirAll(filepath, os.ModePerm); err != nil {
+	if err := os.MkdirAll(file, os.ModePerm); err != nil {
 		return err
 	}
-	
+
 	// Create the file
-	out, err := os.Create(filepath + "/" + filename)
+	out, err := os.Create(file)
 	if err != nil {
 		return err
 	}
@@ -27,21 +28,21 @@ func Download(filepath string, filename string, url string) (err error) {
 	// Get the data
 	resp, err := http.Get(url)
 	if err != nil {
-		os.Remove(filepath + "/" + filename)
+		os.Remove(file)
 		return err
 	}
 	defer resp.Body.Close()
 
 	// Check server response
 	if resp.StatusCode != http.StatusOK {
-		os.Remove(filepath + "/" + filename)
+		os.Remove(file)
 		return fmt.Errorf("failed to download: %s bad status: %s", url, resp.Status)
 	}
 
 	// Writer the body to file
 	_, err = io.Copy(out, resp.Body)
 	if err != nil {
-		os.Remove(filepath + "/" + filename)
+		os.Remove(file)
 		return err
 	}
 	return
